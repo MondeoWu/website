67fc810f23b1c4c8effc67a2c81892636a6aa5d3
Miles-Yang <miles@interactivelabs.co>
2020-04-07 12:56:47 +0800

    add kcors

--- api/index.ts
+++ api/index.ts
@@ -2,10 +2,16 @@ import './config'
 import * as Koa from 'koa'
 import { useMidlewares } from './middlewares'
 import sequelize from './db/database'
+import * as cors from 'kcors'
 
 sequelize.sync().then(() => {
   const port = process.env.PORT || 3333
   const app = new Koa()
+  app.use(
+    cors({
+      maxAge: 86400
+    })
+  )
   useMidlewares(app)
   app.listen(port, () => {
     console.log(`app.listen(${port})`)
