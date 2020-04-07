f6108ff6e1327869fe9863c66b79afc036eed1d6
Miles-Yang <miles@interactivelabs.co>
2020-04-07 16:15:26 +0800

    add cors

--- api/middlewares/index.ts
+++ api/middlewares/index.ts
@@ -2,8 +2,14 @@ import logger from './logger'
 import routers from '../routers'
 import * as jwt from 'koa-jwt'
 import constants from '../config/constant'
+import * as cors from 'kcors'
 
 export const useMidlewares = (app) => {
+    app.use(
+        cors({
+          maxAge: 86400,
+        })
+      )
     app.use(logger())
     app.use(jwt({ secret: process.env.JWT_SECRET }).unless({ path:  constants.publicApis }))
     app.use(routers.middleware())
